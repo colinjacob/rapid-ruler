97c18b31c10dc7f5988c3b6f11cbeac2a553eed6
colinjacob <colin.j.messger@gmail.com>
2018-04-10 10:14:48 -0500

    Meaningful commit

--- wp-login.php
+++ wp-login.php
@@ -9,19 +9,19 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Log In &lsaquo; rapidrule &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' href='https://colinjacob.github.io/rapidruler/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=5.0-alpha-42964' type='text/css' media='all' />
+<link rel='stylesheet' href='https://colinjacob.github.io/rapid-ruler/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=5.0-alpha-42964' type='text/css' media='all' />
 <meta name='robots' content='noindex,follow' />
 	<meta name="viewport" content="width=device-width" />
-	<link rel="icon" href="https://colinjacob.github.io/rapidruler/wp-content/uploads/2018/04/rapidrulelogo-150x76.png" sizes="32x32" />
-<link rel="icon" href="https://colinjacob.github.io/rapidruler/wp-content/uploads/2018/04/rapidrulelogo.png" sizes="192x192" />
-<link rel="apple-touch-icon-precomposed" href="https://colinjacob.github.io/rapidruler/wp-content/uploads/2018/04/rapidrulelogo.png" />
-<meta name="msapplication-TileImage" content="https://colinjacob.github.io/rapidruler/wp-content/uploads/2018/04/rapidrulelogo.png" />
+	<link rel="icon" href="https://colinjacob.github.io/rapid-ruler/wp-content/uploads/2018/04/rapidrulelogo-150x76.png" sizes="32x32" />
+<link rel="icon" href="https://colinjacob.github.io/rapid-ruler/wp-content/uploads/2018/04/rapidrulelogo.png" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="https://colinjacob.github.io/rapid-ruler/wp-content/uploads/2018/04/rapidrulelogo.png" />
+<meta name="msapplication-TileImage" content="https://colinjacob.github.io/rapid-ruler/wp-content/uploads/2018/04/rapidrulelogo.png" />
 	</head>
 	<body class="login login-action-login wp-core-ui  locale-en-us">
 		<div id="login">
 		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">Powered by WordPress</a></h1>
 	
-	<form name="loginform" id="loginform" action="https://colinjacob.github.io/rapidruler/wp-login.php" method="post">
+	<form name="loginform" id="loginform" action="https://colinjacob.github.io/rapid-ruler/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Username or Email Address<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" /></label>
@@ -33,13 +33,13 @@
 		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
 	<p class="submit">
 		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
-			<input type="hidden" name="redirect_to" value="https://colinjacob.github.io/rapidruler/wp-admin/" />
+			<input type="hidden" name="redirect_to" value="https://colinjacob.github.io/rapid-ruler/wp-admin/" />
 				<input type="hidden" name="testcookie" value="1" />
 	</p>
 	</form>
 
 		<p id="nav">
-			<a href="https://colinjacob.github.io/rapidruler/wp-login.php?action=lostpassword">Lost your password?</a>
+			<a href="https://colinjacob.github.io/rapid-ruler/wp-login.php?action=lostpassword">Lost your password?</a>
 		</p>
 	
 	<script type="text/javascript">
@@ -56,7 +56,7 @@
 		if(typeof wpOnload=='function')wpOnload();
 		</script>
 
-		<p id="backtoblog"><a href="https://colinjacob.github.io/rapidruler/">
+		<p id="backtoblog"><a href="https://colinjacob.github.io/rapid-ruler/">
 											&larr; Back to rapidrule	</a></p>
 	
 	</div>
